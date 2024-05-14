4102337770a5374c3a3a7dc776adaea5803ae112
Truong Ngo <30459021+ngojohn2002@users.noreply.github.com>
2024-05-14 02:22:11 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-const apiKey = `at_jNLQYIanueylgMsqFyEW3vxR8ZebE`;
+const apiKey = `at_9KS02gBH2SXFd3z6A3DAisbYXKSBC`;
 
 function checkDomain() {
   const domain = document.getElementById(`domainInput`).value.trim();
   if (!domain) {
-    showModal(`Please enter a domain name.`);
+    showModal(`<strong>Please enter a domain name.</strong>`);
     return;
   }
 
   fetch(
-    `https://domain-availability.whoisxmlapi.com/api/v1?apiKey=${apiKey}&domainName=${domain}&credits=DA&outputFormat=JSON`
+    `https://domain-availability.whoisxmlapi.com/api/v1?apiKey=${apiKey}&domainName=${domain}&outputFormat=JSON`
   )
     .then((response) => response.json())
     .then((data) => {
@@ -20,7 +20,7 @@ function checkDomain() {
       if (data.DomainInfo.domainAvailability === `UNAVAILABLE`) {
         getRegistrantInfo(domain);
       } else {
-        showModal(`Domain "${domain}" is available.`);
+        showModal(`<strong>Domain <h2>"${domain}"</h2> is available.</strong>`);
       }
     })
     .catch((error) => console.error(`Error:`, error));
@@ -33,22 +33,81 @@ function getRegistrantInfo(domain) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      console.log(
-        `data.WhoisRecord.administrativeContact.rawText: ` +
-          data.WhoisRecord.administrativeContact.rawText
-      );
+      console.log(`
+        Domain name: ${data.WhoisRecord.domainName}
+        Domain availability: ${data.WhoisRecord.domainAvailability}
+
+        Created date: ${data.WhoisRecord.createdDateNormalized}
+        Expires date: ${data.WhoisRecord.expiresDateNormalized}
+
+        ====================
+        WhoisRecord.rawText: 
+        ${data.WhoisRecord.rawText}
+        
+        ====================
+        WhoisRecord.registryData.createdDateNormalized: ${data.WhoisRecord.registryData.createdDateNormalized}
+        WhoisRecord.registryData.expiresDateNormalized: ${data.WhoisRecord.registryData.expiresDateNormalized}
+        WhoisRecord.registryData.updatedDateNormalized: ${data.WhoisRecord.registryData.updatedDateNormalized}
+
+        ====================
+        WhoisRecord.registryData.rawText: 
+        ${data.WhoisRecord.registryData.rawText}
+
+        ====================
+        WhoisRecord.registrant.rawText: 
+        ${data.WhoisRecord.registrant.rawText}
+
+        ====================
+        WhoisRecord.administrativeContact.rawText: 
+        ${data.WhoisRecord.administrativeContact.rawText}
+        `);
       const registrantInfo = data?.WhoisRecord?.registrant;
       if (registrantInfo) {
+        // Replace \n with <br> in raw text data
+        // let registrantInfo = data.WhoisRecord.registrant.rawText;
+        // registrantInfo = registrantInfo.replace(/(?:\r\n|\r|\n)/g, "<br>");
+        // let registryData = data.WhoisRecord.registryData.rawText;
+        // registryData = registryData.replace(/(?:\r\n|\r|\n)/g, "<br>");
+
+        // Replace \n with <br><h3> and colon with </h3> in raw text data
+        // let registrantInfo = data.WhoisRecord.registrant.rawText;
+        // registrantInfo =
+        //   "<h3>" +
+        //   registrantInfo.replace(/\n/g, "<br><h3>").replace(/:/g, "</h3>");
+        // let registryData = data.WhoisRecord.registryData.rawText;
+        // registryData =
+        //   "<h3>" +
+        //   registryData.replace(/\n/g, "<br><h3>").replace(/:/g, "</h3>");
+
+        // Replace \n with <br><strong> and colon with :</strong> in raw text data
+        let registrantInfo = data.WhoisRecord.registrant.rawText;
+        registrantInfo =
+          "<strong>" +
+          registrantInfo
+            .replace(/\n/g, "<br><strong>")
+            .replace(/:/g, ":</strong>");
+        let registryData = data.WhoisRecord.registryData.rawText;
+        registryData =
+          "<strong>" +
+          registryData
+            .replace(/\n/g, "<br><strong>")
+            .replace(/:/g, ":</strong>");
+
         const info = `
-          Domain: ${domain}<br>
-          Registrant Name: ${registrantInfo.name}<br>
-          Registrant Email: ${registrantInfo.email}<br>
-          Registrant Phone: ${registrantInfo.telephone}
+          <strong>Domain name:</strong> ${data.WhoisRecord.domainName}<br>
+          <strong>Domain availability:</strong> ${data.WhoisRecord.domainAvailability}<br>
+          <br>
+          <h2>Registrant info:</h2><br>
+          ${registrantInfo}<br>
+          <br>
+          <h2>Registry data:</h2><br>
+          ${registryData}<br>
+          <br>
         `;
         showModal(info);
         saveToLocalStorage(domain, registrantInfo);
       } else {
-        showModal(`Registrant information not available.`);
+        showModal(`<strong>Registrant information not available.</strong>`);
       }
     })
     .catch((error) => console.error(`Error:`, error));
@@ -67,6 +126,7 @@ function closeModal() {
   document.getElementById(`domainInput`).value = ``;
 }
 
+// Need more details
 function saveToLocalStorage(domain, registrantInfo) {
   const domainData = {
     name: registrantInfo.name,
